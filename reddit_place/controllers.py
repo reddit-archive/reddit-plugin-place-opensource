9f906ce7ad25b62ae6324ac1433b2f30a8aae5e4
Brian Simpson <bsimpson63@gmail.com>
2017-02-06 12:05:16 -0800

    Add validation to POST_draw

--- reddit_place/controllers.py
+++ reddit_place/controllers.py
@@ -1,8 +1,12 @@
+import datetime
+
+from pylons import app_globals as g
 from pylons import tmpl_context as c
 
 from r2.controllers import add_controller
 from r2.controllers.reddit_base import RedditController
 from r2.lib import websockets
+from r2.lib.errors import errors
 from r2.lib.validator import (
     validatedForm,
     VColor,
@@ -12,6 +16,10 @@ from r2.lib.validator import (
 )
 
 from .pages import PlacePage, PlaceCanvasse
+
+
+ACCOUNT_CREATION_CUTOFF = datetime.datetime(2017, 4, 1, 0, 0, tzinfo=g.tz)
+
 
 @add_controller
 class PlaceController(RedditController):
@@ -34,6 +42,25 @@ class PlaceController(RedditController):
         color=VColor("color"),
     )
     def POST_draw(self, form, jquery, x, y, color):
+        if c.user._date >= ACCOUNT_CREATION_CUTOFF:
+            self.abort403()
+
+        if x is None:
+            form.set_error(errors.BAD_NUMBER, "x")
+
+        if y is None:
+            form.set_error(errors.BAD_NUMBER, "y")
+
+        if (form.has_errors("x", errors.BAD_NUMBER) or
+                form.has_errors("y", errors.BAD_NUMBER)):
+            return
+
+        if not color:
+            form.set_error(errors.BAD_COLOR, "color")
+
+        if form.has_errors("color", errors.BAD_COLOR):
+            return
+
         websockets.send_broadcast(
             namespace="/place",
             type="place",
