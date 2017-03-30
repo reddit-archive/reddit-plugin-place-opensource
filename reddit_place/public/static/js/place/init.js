reddit-archive/reddit-plugin-place-opensource

commit 5d69c029e34457b478b1778d45e0af4bb14ef3fc

Replace img tag hack with fullscreen canvas hack

reddit_place/public/static/js/place/init.js
@@ -120,10 +120,72 @@
 
     Client.init(isUserLoggedIn, cooldownDuration, startOffsets.x, startOffsets.y);
 
+    // Some browsers (Safari, Edge) have a blurry canvas problem due to 
+    // lack of proper support for the 'image-rendering' css rule, which is 
+    // what allows us to scale up the canvas without bilinear interpolation.
+    // We can still upscale correctly by drawing the small canvas into a bigger
+    // canvas using the imageSmoothingEnabled flag.
+    var canvasDiv = null;
+    var displayCanvas = null;
+    var displayCtx = null;
+    var usingBlurryCanvasFix = false;
+
+    // Only apply to browsers where this is a known issue.
+    var isSafari = (window.navigator.userAgent.indexOf('Safari') > -1 &&
+                    window.navigator.userAgent.indexOf('Chrome') === -1);
+    var isEdge = window.navigator.userAgent.indexOf('Edge') > -1;
+    if (isSafari || isEdge) {
+      usingBlurryCanvasFix = true;
+      // To avoid having to redo event work, we just let the existing canvas
+      // element sit there invisibly.
+      $(Canvasse.el).css({ opacity: 0 });
+      displayCanvas = document.createElement('canvas');
+      displayCtx = displayCanvas.getContext('2d');
+      $(displayCanvas).addClass('place-display-canvas');
+      $(container).prepend(displayCanvas);
+      resizeDisplayCanvas();
+    }
+
+    function resizeDisplayCanvas() {
+      var containerRect = container.getBoundingClientRect();
+      displayCanvas.width = containerRect.width;
+      displayCanvas.height = containerRect.height;
+      // Here's the magic.  These flags are reset any time the canvas resize
+      // changes, so they need to be set here.
+      displayCtx.mozImageSmoothingEnabled = false;
+      displayCtx.webkitImageSmoothingEnabled = false;
+      displayCtx.msImageSmoothingEnabled = false;
+      displayCtx.imageSmoothingEnabled = false;
+      redrawDisplayCanvas();
+    }
+
+    function redrawDisplayCanvas() {
+      displayCtx.clearRect(0, 0, displayCanvas.width, displayCanvas.height);
+      displayCtx.drawImage(
+        Canvasse.el,
+        // Center the canvas, then apply the current pan offset.  The half pixel
+        // css it necessary to match the same offset applied to the real canvas
+        // via a css transform.
+        (displayCanvas.width / 2) + (Client._panX - halfWidth - .5) * Client._zoom,
+        (displayCanvas.height / 2) + (Client._panY - halfHeight - .5) * Client._zoom,
+        Canvasse.width * Client._zoom,
+        Canvasse.height * Client._zoom
+      );
+    }
+
+    bindEvents(window, {
+      'resize': function() {
+        resizeDisplayCanvas();
+      },
+    });
+
     R2Server.getCanvasBitmapState().then(function(timestamp, canvas) {
       // TODO - request non-cached version if the timestamp is too old
       if (!canvas) { return; }
       Client.setInitialState(canvas);
+      if (usingBlurryCanvasFix) {
+        redrawDisplayCanvas();
+      }
     });
 
     var websocket = new r.WebSocket(websocketUrl);
@@ -207,7 +269,12 @@
     startTicking(function() {
       Client.tick();
       Cursor.tick();
-      Canvasse.tick();
+      var cameraDidUpdate = Camera.tick();
+      var canvasDidUpdate = Canvasse.tick();
+
+      if (usingBlurryCanvasFix && (cameraDidUpdate || canvasDidUpdate)) {
+        redrawDisplayCanvas();
+      }
     });
 
     r.place = Client;

reddit_place/public/static/css/place.less
@@ -33,6 +33,7 @@
 }
 
 .place-canvas {
+    -ms-interpolation-mode: bicubic;
     image-rendering: -moz-crisp-edges;
     image-rendering: -webkit-optimize-contrast;
     image-rendering: crisp-edges; /* so crisp */
@@ -40,8 +41,16 @@
     background-color: white;
     pointer-events: none;
     transform: translate(-.5px, -.5px);
-    // TODO - remove
-    // outline: dashed green;
+    display: block;
+}
+
+.place-display-canvas {
+    position: absolute;
+    pointer-events: none;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
 }
 
 .place-camera:active {

reddit_place/public/static/js/place/camera.js
@@ -5,6 +5,7 @@
   return {
     zoomElement: null,
     panElement: null,
+    isDirty: false,
 
     /**
      * Initialize the camera.
@@ -18,12 +19,21 @@
       this.panElement = panElement;
     },
 
+    tick: function() {
+      if (this.isDirty) {
+        this.isDirty = false;
+        return true;
+      }
+      return false;
+    },
+
     /**
      * Update the scale transform on the zoomElement element.
      * @function
      * @param {number} s The scale
      */
     updateScale: function(s) {
+      this.isDirty = true;
       $(this.zoomElement).css({
         transform: 'scale(' + s + ',' + s + ')',
       });
@@ -36,6 +46,7 @@
      * @param {number} y The vertical offset
      */
     updateTranslate: function(x, y) {
+      this.isDirty = true;
       $(this.panElement).css({
         transform: 'translate(' + x + 'px,' + y + 'px)',
       });

reddit_place/public/static/js/place/canvasse.js
@@ -17,7 +17,6 @@
     height: 0,
     el: null,
     ctx: null,
-    img: null,
     bufferEl: null,
     bufferCtx: null,
     // TODO - not sure if I'll actually need these yet, remove if they aren't
@@ -42,6 +41,10 @@
       this.el.width = width;
       this.el.height = height;
       this.ctx = this.el.getContext('2d');
+      this.ctx.mozImageSmoothingEnabled = false;
+      this.ctx.webkitImageSmoothingEnabled = false;
+      this.ctx.msImageSmoothingEnabled = false;
+      this.ctx.imageSmoothingEnabled = false;
 
       // The actual canvas state.  This canvas is hidden, to allow us to do
       // stuff like pause rendering of incoming updates without losing them,
@@ -60,32 +63,19 @@
       // This view into the buffer is used to write.  Values written should be
       // 32 bit colors stored as AGBR (rgba in reverse).
       this.writeBuffer = new Uint32Array(this.buffer);
-
-      // Safari has a blurry-canvas problem due to its lack of proper
-      // support for the 'image-rendering' css rule, which is what allows
-      // us to scale up the canvas without bilinear interpolation.
-      // Safari does support this on image elements though, so the hack here
-      // is to hide the canvas and use an img tag instead.  This comes with
-      // a performance hit, so we don't want to do it on other platforms.
-      if (window.navigator.userAgent.indexOf('Safari') > -1 &&
-          window.navigator.userAgent.indexOf('Chrome') === -1) {
-        this.img = new Image();
-        this.img.width = width;
-        this.img.height = height;
-        $(this.img).addClass('place-canvas');
-        $(this.el).parent().append(this.img);
-        $(this.el).detach();
-      }
     },
 
     /**
      * Tick function that draws buffered updates to the display.
      * @function
+     * @returns {boolean} Returns true if any updates were made
      */
     tick: function() {
       if (this.isBufferDirty) {
         this.drawBufferToDisplay();
+        return true;
       }
+      return false;
     },
 
     /**
@@ -190,10 +180,6 @@
       var imageData = new ImageData(this.readBuffer, this.width, this.height);
       this.ctx.putImageData(imageData, 0, 0);
       this.isBufferDirty = false;
-
-      if (this.img) {
-        this.img.src = this.el.toDataURL();
-      }
     },
   };
 });

reddit_place/public/static/js/place/client.js
@@ -284,11 +284,14 @@
      * Tick function that updates interpolated zoom and offset values.
      * Not intended for external use.
      * @function
+     * @returns {boolean} Returns true if anything updated.
      */
     tick: function() {
+      var didUpdate = false;
       if (this._zoom !== this.zoom) {
         this._zoom = lerp(this._zoom, this.zoom, this.ZOOM_LERP_SPEED);
         Camera.updateScale(this._zoom);
+        didUpdate = true;
       }
 
       this.panX += this.currentDirection.dx;
@@ -305,9 +308,13 @@
         didOffsetUpdate = true;
       }
 
+      didUpdate = didUpdate || didOffsetUpdate;
+
       if (didOffsetUpdate) {
         Camera.updateTranslate(this._panX, this._panY);
       }
+
+      return didUpdate;
     },
 
     /**

commit fcfe74d3d7c0934cefe3090935ae614b7ba46ad5

Fix error when resizing window in Chrome or Firefox
5d69c02 introduced a new fix for the blurry image issue on Safari and
Edge, but also introduced a bug on browsers that don't need the fix.

reddit_place/public/static/js/place/init.js
@@ -162,6 +162,12 @@
       $(displayCanvas).addClass('place-display-canvas');
       $(container).prepend(displayCanvas);
       resizeDisplayCanvas();
+
+      bindEvents(window, {
+        'resize': function() {
+          resizeDisplayCanvas();
+        },
+      });
     }
 
     function resizeDisplayCanvas() {
@@ -190,12 +196,6 @@
         Canvasse.height * Client._zoom
       );
     }
-
-    bindEvents(window, {
-      'resize': function() {
-        resizeDisplayCanvas();
-      },
-    });
 
     R2Server.getCanvasBitmapState().then(function(timestamp, canvas) {
       // TODO - request non-cached version if the timestamp is too old