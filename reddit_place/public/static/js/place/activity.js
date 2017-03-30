1703555b6832248850dfbf289a7ab285749594a0
Matt Lee <matt@reddit.com>
2017-03-30 17:27:10 -0400

    Format the activity count display text

--- reddit_place/public/static/js/place/activity.js
+++ reddit_place/public/static/js/place/activity.js
@@ -1,4 +1,20 @@
 !r.placeModule('activity', function(require) {
+  function formatCount(count) {
+    if (count >= 1000000) {
+      // I don't think we need to worry about more than this :P
+      return (count / 1000000).toFixed(2) + 'm';
+    }
+    if (count >= 100000) {
+      // Drop the decimal once we're into the 100k range
+      return parseInt((count / 1000), 10) + 'k';
+    }
+    if (count >= 1100) {
+      // Show actual numbers up until we'd show at least 1.1k
+      return (count / 1000).toFixed(1) + 'k';
+    }
+    return count.toString();
+  }
+
   return {
     $el: null,
     initialized: false,
@@ -26,7 +42,7 @@
       if (!this.initialized) { return; }
       if (count === this.lastCount) { return; }
       this.lastCount = count;
-      this.$el.text(count);
+      this.$el.text(formatCount(count));
     },
   };
 });
